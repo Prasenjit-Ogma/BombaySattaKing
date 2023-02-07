0e97d6c0c08f496c7a180d867dfba309e8aaa319
Prasenjit Saha <prasenjit.saha@ogmaconceptions.com>
2023-02-07 21:05:05 +0530

    gif work

--- app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
+++ app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
@@ -3,13 +3,6 @@ package com.ogmatechlab.bombaysattaking
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.load.resource.gif.GifDrawable
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
 import com.ogmatechlab.bombaysattaking.databinding.ActivityPlayGameBinding
 
 class PlayGame : AppCompatActivity() {
@@ -26,13 +19,13 @@ class PlayGame : AppCompatActivity() {
         Glide.with(this).asGif().load(R.raw.gif_up).into(playGameBinding.imgAnimation6)
 
         playGameBinding.btn.setOnClickListener {
-            /*Glide.with(this).asBitmap().load(R.raw.gif_down).into(playGameBinding.imgAnimation1)
+            Glide.with(this).asBitmap().load(R.drawable.five).into(playGameBinding.imgAnimation1)
             Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation2)
             Glide.with(this).asBitmap().load(R.raw.gif_down).into(playGameBinding.imgAnimation3)
             Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation4)
             Glide.with(this).asBitmap().load(R.raw.gif_down).into(playGameBinding.imgAnimation5)
-            Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation6)*/
-            Glide.with(this)
+            Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation6)
+            /*Glide.with(this)
                 .asGif()
                 .load(R.raw.gif_down)
                 .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
@@ -53,11 +46,11 @@ class PlayGame : AppCompatActivity() {
                         dataSource: DataSource?,
                         isFirstResource: Boolean
                     ): Boolean {
-                        resource?.setLoopCount(2)
+
                         return false
                     }
 
-                }).into(playGameBinding.imgAnimation1)
+                }).into(playGameBinding.imgAnimation1)*/
         }
 
     }
