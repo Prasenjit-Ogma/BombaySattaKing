153a46dee81d9150cb7c99dc9cd901e9e9d3b702
Prasenjit Saha <prasenjit.saha@ogmaconceptions.com>
2023-02-07 18:01:43 +0530

    gif work

--- app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
+++ app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
@@ -2,6 +2,7 @@ package com.ogmatechlab.bombaysattaking
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.ogmatechlab.bombaysattaking.databinding.ActivityPlayGameBinding
 
 class PlayGame : AppCompatActivity() {
@@ -10,5 +11,11 @@ class PlayGame : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         playGameBinding = ActivityPlayGameBinding.inflate(layoutInflater)
         setContentView(playGameBinding.root)
+        Glide.with(this).load(R.raw.gif_down).into(playGameBinding.imgAnimation1)
+        Glide.with(this).load(R.raw.gif_up).into(playGameBinding.imgAnimation2)
+        Glide.with(this).load(R.raw.gif_down).into(playGameBinding.imgAnimation3)
+        Glide.with(this).load(R.raw.gif_up).into(playGameBinding.imgAnimation4)
+        Glide.with(this).load(R.raw.gif_down).into(playGameBinding.imgAnimation5)
+        Glide.with(this).load(R.raw.gif_up).into(playGameBinding.imgAnimation6)
     }
 }
