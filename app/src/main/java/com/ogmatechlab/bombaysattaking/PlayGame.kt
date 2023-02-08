ef9e2a5c3aca99206510c460f80a7f1e3bec0f95
Prasenjit Saha <prasenjit.saha@ogmaconceptions.com>
2023-02-08 16:18:59 +0530

    numbers added

--- app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
+++ app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
@@ -17,40 +17,17 @@ class PlayGame : AppCompatActivity() {
         Glide.with(this).asGif().load(R.raw.gif_up).into(playGameBinding.imgAnimation4)
         Glide.with(this).asGif().load(R.raw.gif_down).into(playGameBinding.imgAnimation5)
         Glide.with(this).asGif().load(R.raw.gif_up).into(playGameBinding.imgAnimation6)
+        Glide.with(this).asGif().load(R.raw.gif_color_down).into(playGameBinding.imgAnimation7)
+        Glide.with(this).asGif().load(R.raw.gif_color_up).into(playGameBinding.imgAnimation8)
 
         playGameBinding.btn.setOnClickListener {
-            Glide.with(this).asBitmap().load(R.drawable.five).into(playGameBinding.imgAnimation1)
+            Glide.with(this).asBitmap().load(R.drawable.num_five)
+                .into(playGameBinding.imgAnimation1)
             Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation2)
             Glide.with(this).asBitmap().load(R.raw.gif_down).into(playGameBinding.imgAnimation3)
             Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation4)
             Glide.with(this).asBitmap().load(R.raw.gif_down).into(playGameBinding.imgAnimation5)
             Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation6)
-            /*Glide.with(this)
-                .asGif()
-                .load(R.raw.gif_down)
-                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
-                .listener(object : RequestListener<GifDrawable> {
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<GifDrawable>?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        return false
-                    }
-
-                    override fun onResourceReady(
-                        resource: GifDrawable?,
-                        model: Any?,
-                        target: Target<GifDrawable>?,
-                        dataSource: DataSource?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-
-                        return false
-                    }
-
-                }).into(playGameBinding.imgAnimation1)*/
         }
 
     }
