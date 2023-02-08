aa107e03dc1fa8eba591657fb26cb122231f18ad
Prasenjit Saha <prasenjit.saha@ogmaconceptions.com>
2023-02-08 18:19:58 +0530

    design fixing

--- app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
+++ app/src/main/java/com/ogmatechlab/bombaysattaking/PlayGame.kt
@@ -1,12 +1,18 @@
 package com.ogmatechlab.bombaysattaking
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.ogmatechlab.bombaysattaking.databinding.ActivityPlayGameBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class PlayGame : AppCompatActivity() {
     private lateinit var playGameBinding: ActivityPlayGameBinding
+    private lateinit var formatter: DateTimeFormatter
+    private lateinit var current: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         playGameBinding = ActivityPlayGameBinding.inflate(layoutInflater)
@@ -30,5 +36,25 @@ class PlayGame : AppCompatActivity() {
             Glide.with(this).asBitmap().load(R.raw.gif_up).into(playGameBinding.imgAnimation6)
         }
 
+        fetchTimeFromDevice()
+
     }
+
+    private fun fetchTimeFromDevice() {
+        val timer = object : CountDownTimer(60000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+                current = LocalDateTime.now().format(formatter)
+                playGameBinding.txtTimeStamp.text = current
+                Log.e("PRINT", current)
+                //checkCurrentTime()
+            }
+
+            override fun onFinish() {
+                fetchTimeFromDevice()
+            }
+        }
+        timer.start()
+    }
+
 }
